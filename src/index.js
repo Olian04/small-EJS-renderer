17839157e472d5489dedda74afb258dc7f1eb90b
Oliver Anteros <rift95@live.se>
2019-10-20 23:34:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const render = (template, data) => {
-	template = template.replace(/<%=(.*?)%>/gm, '${$1}');
+  template = template.replace(/<%=(.*?)%>/gm, '${$1}');
   const templateFragments = template.split(/<%.*?%>/gm);
   const codeFragments = template.match(/<%.*?%>/gm).map(m => m.substring(2, m.length-2));
   let t = 0, c = 0;
   return eval(`(() => {
-  	${Object.keys(data).map(k => `var ${k} = ${JSON.stringify(data[k])};`).join('\n')}
+    ${Object.keys(data).map(k => `var ${k} = ${JSON.stringify(data[k])};`).join('\n')}
     var result = '';
     ${Array(templateFragments.length + codeFragments.length).fill(0).reduce((code, _, i) => {
       if (i % 2 === 0) {
